955ec5fd6f9077e5345836c7923e113505dad6c6
David L Bueno <dbueno@bethel.jw.org>
2023-04-14 11:48:10 -0400

    Add filtering buttons above exercises table

--- exercises.php
+++ exercises.php
@@ -27,45 +27,58 @@
       <table>
         <tbody>
         <?php
-          require_once('db.php');
-          // get all distinct muscle names that are used in the query
-          $query_muscles = "SELECT DISTINCT muscles.name FROM exercises 
-          INNER JOIN exercise_muscles ON exercises.id = exercise_muscles.exercise_id 
-          INNER JOIN muscles ON exercise_muscles.muscle_id = muscles.id";
-          $result_muscles = query($query_muscles);
-          $muscles = array();
-          while ($row_muscles = mysqli_fetch_assoc($result_muscles)) {
-            $muscles[] = $row_muscles['name'];
-          }
-          // build the query for exercise and muscle data
-          $query = "SELECT exercises.id, exercises.name, exercises.difficulty, ";
-          foreach ($muscles as $muscle) {
-            $query .= "MAX(CASE WHEN muscles.name = '$muscle' THEN exercise_muscles.intensity ELSE NULL END) AS $muscle, ";
-          }
-          $query = rtrim($query, ", "); // remove the trailing comma and space
-          $query .= " FROM exercises 
-          INNER JOIN exercise_muscles ON exercises.id = exercise_muscles.exercise_id 
-          INNER JOIN muscles ON exercise_muscles.muscle_id = muscles.id 
-          GROUP BY exercises.id";
-          // execute the query and output the results
-          $result = query($query);
-          echo "<table>";
-          echo "<tr><th>Name</th><th>Difficulty</th>";
-          foreach ($muscles as $muscle) {
-            echo "<th>$muscle</th>";
-          }
-          echo "</tr>";
-          while ($row = mysqli_fetch_assoc($result)) {
-            echo "<tr>";
-            echo "<td>" . $row['name'] . "</td>";
-            echo "<td>" . $row['difficulty'] . "</td>";
-            foreach ($muscles as $muscle) {
-              echo "<td>" . $row[$muscle] . "</td>";
-            }
-            echo "</tr>";
-          }
-          echo "</table>";
-          ?>
+require_once('db.php');
+
+// check if a filter has been selected
+$filter = isset($_GET['filter']) ? $_GET['filter'] : 'all';
+// get all distinct muscle names that are used in the query
+$query_muscles = "SELECT DISTINCT muscles.name FROM exercises 
+INNER JOIN exercise_muscles ON exercises.id = exercise_muscles.exercise_id 
+INNER JOIN muscles ON exercise_muscles.muscle_id = muscles.id";
+$result_muscles = query($query_muscles);
+$muscles = array();
+while ($row_muscles = mysqli_fetch_assoc($result_muscles)) {
+  $muscles[] = $row_muscles['name'];
+}
+// build the query for exercise and muscle data
+$query = "SELECT exercises.id, exercises.name, exercises.difficulty, ";
+foreach ($muscles as $muscle) {
+  $query .= "MAX(CASE WHEN muscles.name = '$muscle' THEN exercise_muscles.intensity ELSE NULL END) AS $muscle, ";
+}
+$query = rtrim($query, ", "); // remove the trailing comma and space
+$query .= " FROM exercises 
+INNER JOIN exercise_muscles ON exercises.id = exercise_muscles.exercise_id 
+INNER JOIN muscles ON exercise_muscles.muscle_id = muscles.id";
+// apply the filter if one has been selected
+if ($filter !== 'all') {
+  $query .= " WHERE exercises.type = '$filter'";
+}
+$query .= " GROUP BY exercises.id";
+// execute the query and output the results
+$result = query($query);
+echo "<div>
+  <button onclick=\"location.href='?filter=all'\">All Exercises</button>
+  <button onclick=\"location.href='?filter=push'\">Push Exercises</button>
+  <button onclick=\"location.href='?filter=pull'\">Pull Exercises</button>
+  <button onclick=\"location.href='?filter=legs'\">Legs Exercises</button>
+</div>";
+echo "<table>";
+echo "<tr><th>Name</th><th>Difficulty</th>";
+foreach ($muscles as $muscle) {
+  echo "<th>$muscle</th>";
+}
+echo "</tr>";
+while ($row = mysqli_fetch_assoc($result)) {
+  echo "<tr>";
+  echo "<td>" . $row['name'] . "</td>";
+  echo "<td>" . $row['difficulty'] . "</td>";
+  foreach ($muscles as $muscle) {
+    echo "<td>" . $row[$muscle] . "</td>";
+  }
+  echo "</tr>";
+}
+echo "</table>";
+?>
         </tbody>
       </table>
     </div>
