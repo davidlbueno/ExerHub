87995424eab79e30b1fefaa8aa443c798937e56e
David L Bueno <dbueno@bethel.jw.org>
2023-04-18 00:25:34 -0400

    Nav improvements, create workout button

--- workouts.php
+++ workouts.php
@@ -10,16 +10,23 @@
   <?php require_once 'db.php'; ?>
 </head>
 <body class="dark">
-  <nav>
-    <div class="nav-wrapper">
-      <a href="index.html" class="brand-logo">BWE</a>
-      <a href="index.html" data-target="side-nav" class="show-on-large sidenav-trigger"><i class="material-icons">menu</i></a>
-      <ul class="right" id="top-nav"></ul>
-    </div>
-  </nav>
+<nav>
+  <div class="nav-wrapper">
+    <a href="index.html" class="brand-logo" style="margin-left: 60px;">BWE: Workouts</a>
+    <a href="index.html" data-target="side-nav" class="show-on-large sidenav-trigger"><i class="material-icons">menu</i></a>
+    <ul class="right" id="top-nav"></ul>
+  </div>
+</nav>
+
   <ul class="sidenav" id="side-nav"></ul>
   <main class="container">
-    <h2>Workouts</h2>
+  <div class="row">
+    <div class="col s8">
+    </div>
+    <div class="col s2" style="width: 174px; align-items: center; margin-top: 10px;">
+      <a href="#" class="btn btn-floating waves-effect waves-light"><i class="material-icons">add</i></a><span style="margin-left: 5px;">Create Workout</span>
+    </div>
+  </div>
   </main>
   <script src="nav.js"></script>
 </body>
