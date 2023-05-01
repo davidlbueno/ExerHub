e54e196a65dd7cd6bfd5ddc3d755ece4d1d5c5f0
David L Bueno <dbueno@bethel.jw.org>
2023-05-01 11:25:36 -0400

    Added workout name field

--- bwe.js
+++ bwe.js
@@ -1,8 +1,8 @@
-const itemSelect = document.getElementById("item-select");
-const exerciseSelect = document.getElementById("exercise-select");
-const setsSelect = document.getElementById("sets-select");
-const addItemBtn = document.getElementById("add-item-btn");
-const itemsList = document.getElementById("items-list");
+let itemSelect = document.getElementById("item-select");
+let exerciseSelect = document.getElementById("exercise-select");
+let setsSelect = document.getElementById("sets-select");
+let addItemBtn = document.getElementById("add-item-btn");
+let itemsList = document.getElementById("items-list");
 itemSelect.addEventListener("change", () => {
   const selectedType = itemSelect.value;
   if (selectedType) {
@@ -11,57 +11,40 @@ itemSelect.addEventListener("change", () => {
     xhr.onload = () => {
       if (xhr.status === 200) {
         const exercises = JSON.parse(xhr.responseText);
-        exerciseSelect.innerHTML = "<option value='' disabled selected>Exercise</option>";
-        exercises.forEach(exercise => {
-          const option = document.createElement("option");
-          option.value = exercise.name;
-          option.textContent = exercise.name;
-          if (selectedType !== 'Rest') {
-            exerciseSelect.appendChild(option);
-            exerciseSelect.disabled = false;
-            setsSelect.disabled = false;
-          }
-        });
-        if (selectedType === 'Rest') {
-          exerciseSelect.disabled = true;
-          setsSelect.disabled = true;
-        }
+        exerciseSelect.innerHTML = `
+        <option value="" disabled selected>Exercise</option>
+        ${exercises.map(exercise => `
+            <option value="${exercise.name}">${exercise.name}</option>
+        `).join('')}
+        `;
+        exerciseSelect.disabled = selectedType === 'Rest';
+        setsSelect.disabled = selectedType === 'Rest';
       }
     };
     xhr.send();
-  } else {
-    exerciseSelect.innerHTML = "<option value='' disabled selected>Exercise</option>";
-  }
+  } 
 });
-document.getElementById("item-select").focus();
+document.getElementById("workout-name").focus();
 addItemBtn.addEventListener("click", () => {
-  const itemValue = itemSelect.value;
-  const exerciseValue = exerciseSelect.value;
-  const secondsInput = document.querySelector('input[name="seconds"]');
-  const setsInput = document.querySelector('input[name="sets"]');
-  const secondsValue = secondsInput.value;
-  const setsValue = setsInput.value;
-  const $addItemBtn = $('#add-item-btn');
-  const selectedListItem = $(".selected");
-  const itemsArray = [];
-  if (itemValue === "Rest" && secondsValue) {
-    if (!$(".selected").length > 0) {
-      itemsArray.push({
-        item: itemValue,
-        seconds: secondsValue,
+  let secondsInput = document.querySelector('input[name="seconds"]');
+  let setsInput = document.querySelector('input[name="sets"]');
+  let $addItemBtn = $('#add-item-btn');
+  let selectedListItem = $(".selected");
+  let itemsArray = [];
+  let newItem = document.createElement("li");
+  if (itemSelect.value === "Rest" && secondsInput.value) {
+    itemsArray.push({
+        item: itemSelect.value,
+        seconds: secondsInput.value,
       });
-      const newItem = document.createElement("li");
-      newItem.innerHTML = `${itemValue} - (${secondsValue}s)`;
+    if (!$(".selected").length > 0) {
+      newItem.innerHTML = `${itemSelect.value} - (${secondsInput.value}s)`;
       newItem.style.backgroundColor = "#454500";
       itemsList.appendChild(newItem);
       itemSelect.value = "";
       secondsInput.value = "";
     } else {
-        itemsArray.push({
-          item: itemValue,
-          seconds: secondsValue,
-        });
-        selectedListItem.html(`${itemValue} - (${secondsValue}s)`)
+        selectedListItem.html(`${itemSelect.value} - (${secondsInput.value}s)`)
         itemSelect.value = "";
         secondsInput.value = "";
         selectedListItem.css('background-color', '#454500');
@@ -70,40 +53,30 @@ addItemBtn.addEventListener("click", () => {
   } else {
       exerciseSelect.disabled = false;
       setsSelect.disabled = false;
-      if (itemValue && exerciseValue && secondsValue && setsValue) {
-        if (!$(".selected").length > 0) {
+      if (itemSelect.value && exerciseSelect.value && secondsInput.value && setsInput.value) {
         itemsArray.push({
-          item: itemValue,
-          exercise: exerciseValue,
-          seconds: secondsValue,
-          sets: setsValue,
+            item: itemSelect.value,
+            exercise: exerciseSelect.value,
+            seconds: secondsInput.value,
+            sets: setsInput.value,
         });
+        if (!$(".selected").length > 0) {
         const newItem = document.createElement("li");
-        newItem.innerHTML = `${itemValue} - ${exerciseValue} (${secondsValue}s, ${setsValue} sets)`;
+        newItem.innerHTML = `${itemSelect.value} - ${exerciseSelect.value} (${secondsInput.value}s, ${setsInput.value} sets)`;
         const newNumber = itemsList.children.length + 1;
         newItem.setAttribute('value', newNumber);
         itemsList.appendChild(newItem);
-        itemSelect.value = "";
-        exerciseSelect.value = "";
-        secondsInput.value = "";
-        setsInput.value = "";
         } else {
-          itemsArray.push({
-            item: itemValue,
-            exercise: exerciseValue,
-            seconds: secondsValue,
-            sets: setsValue,
-          });
-          selectedListItem.html(`${itemValue} - ${exerciseValue} (${secondsValue}s, ${setsValue} sets)`)
-          itemSelect.value = "";
-          exerciseSelect.value = "";
-          secondsInput.value = "";
-          setsInput.value = "";
+          selectedListItem.html(`${itemSelect.value} - ${exerciseSelect.value} (${secondsInput.value}s, ${setsInput.value} sets)`)
           selectedListItem.css('background-color', '#3d3d3d');
           selectedListItem.removeClass('selected');      }
       } else {
         alert("Please enter all required information.");
       }
+      itemSelect.value = "";
+      exerciseSelect.value = "";
+      secondsInput.value = "";
+      setsInput.value = "";
     }
   $addItemBtn.text('Add Item'); 
   document.getElementById("item-select").focus();
@@ -126,7 +99,6 @@ $(document).on('click', "#items-list li", function(event) {
     const exerciseValue = itemText.split(' - ')[1].split(' (')[0];
     const secondsValue = (itemValue === "Rest") ? itemText.split('(')[1].split('s)')[0] : itemText.split(' (')[1].split('s, ')[0];
     const setsValue = (itemValue === "Rest") ? 0 : itemText.split(' (')[1].split('s, ')[1].split(' sets)')[0];
-    const itemSelect = document.getElementById("item-select");
     const secondsInput = document.querySelector('input[name="seconds"]');
     const setsInput = document.querySelector('input[name="sets"]');
     const $addItemBtn = $('#add-item-btn');
@@ -141,22 +113,6 @@ $(document).on('click', "#items-list li", function(event) {
     } else {
       exerciseSelect.disabled = false;
       setsSelect.disabled = false;
-      exerciseSelect.innerHTML = "<option value='' disabled selected>Exercise</option>";
-      const xhr = new XMLHttpRequest();
-      xhr.open("GET", `get_exercises.php?type=${itemValue}`, true);
-      xhr.onload = () => {
-        if (xhr.status === 200) {
-          const exercises = JSON.parse(xhr.responseText);
-          exercises.forEach(exercise => {
-            const option = document.createElement("option");
-            option.value = exercise.name;
-            option.textContent = exercise.name;
-            exerciseSelect.appendChild(option);
-            exerciseSelect.value = exerciseValue;
-          });
-        }
-      };
-      xhr.send();
       setsInput.value = setsValue;
     }
     const itemsArray = [];
@@ -174,16 +130,13 @@ $(document).on('click', "#items-list li.selected", function(event) {
   setsSelect.disabled = false;
   $addItemBtn.text('Add Item');
 });
-
 const outputListItemsBtn = document.querySelector("#output-list-items-btn");
-if (outputListItemsBtn) {
-  outputListItemsBtn.addEventListener("click", () => {
-    const itemsList = document.getElementById("items-list");
-    const items = itemsList.children;
-    for (let i = 0; i < items.length; i++) {
-      console.log(items[i].textContent);
-    }
-  });
-} else {
-  console.log("The element with the id 'output-list-items-btn' does not exist.");
-}
+outputListItemsBtn.addEventListener("click", () => {
+  const workoutName = document.getElementById("workout-name").value;
+  const itemsList = document.getElementById("items-list");
+  const items = itemsList.children;
+  console.log(workoutName);
+  for (let i = 0; i < items.length; i++) {
+    console.log(items[i].textContent);
+  }
+});
