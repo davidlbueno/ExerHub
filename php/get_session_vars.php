dab4957e2e1e4830c1b4424d99126666fc81bbdc
David L Bueno <dbueno@bethel.jw.org>
2023-05-02 11:09:41 -0400

    fixed get_session_vars.php

--- php/get_session_vars.php
+++ php/get_session_vars.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
-
-//$isLoggedIn = isset($_SESSION['user_id']) && isset($_SESSION['user_name']);
 $userName = $_SESSION['user_name'];
-
-// Return the user status as JSON
-//echo json_encode(['isLoggedIn' => $isLoggedIn]);
-echo json_encode(['userName' => $userName]);
+$sessionData = $_SESSION;
+$response = [
+  'sessionData' => $sessionData,
+  'userName' => $userName
+];
+echo json_encode($response);
 ?>
