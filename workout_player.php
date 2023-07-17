45bc9dc1914dea10074f1603ce9961263f711c99
David L Bueno <dbueno@bethel.jw.org>
2023-07-17 00:56:50 -0400

    Switched voice to Amazon Polly Kendra

--- workout_player.php
+++ workout_player.php
@@ -5,6 +5,7 @@
   <title>ExerHub - Workout Player</title>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  <script src="https://sdk.amazonaws.com/js/aws-sdk-2.958.0.min.js"></script>
   <link rel="stylesheet" href="style.css">
   <?php require_once 'php/db.php'; ?>
 </head>
