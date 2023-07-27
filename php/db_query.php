4585fde037e6692dbfe1df8a1e88d70515d28bf3
David L Bueno <davidlbueno@gmail.com>
2023-07-27 13:21:40 -0400

    updated db_query.php

--- php/db_query.php
+++ php/db_query.php
@@ -3,26 +3,29 @@ function query($conn, $query, $params = []) {
   // Prepare the SQL statement
   $stmt = mysqli_prepare($conn, $query);
   if (!$stmt) {
-      error_log("Statement preparation failed: " . mysqli_error($conn));
-      die("An error occurred: " . mysqli_error($conn));
+    return ['success' => false, 'error' => "Statement preparation failed: " . mysqli_error($conn)];
   }
 
   // Bind parameters to the SQL statement
   if (!empty($params)) {
-      $types = str_repeat('s', count($params)); // assumes all parameters are strings
-      mysqli_stmt_bind_param($stmt, $types, ...$params);
+    $types = str_repeat('s', count($params)); // assumes all parameters are strings
+    mysqli_stmt_bind_param($stmt, $types, ...$params);
   }
 
   // Execute the SQL statement
   $result = mysqli_stmt_execute($stmt);
   if (!$result) {
-      error_log("Query execution failed: " . mysqli_stmt_error($stmt));
-      die("An error occurred: " . mysqli_stmt_error($stmt));
+    return ['success' => false, 'error' => "Query execution failed: " . mysqli_stmt_error($stmt)];
+  }
+
+  // For INSERT queries, get the last insert id
+  if (mysqli_stmt_field_count($stmt) === 0) {
+    $result = ['success' => true, 'insert_id' => mysqli_stmt_insert_id($stmt)];
   }
 
   // For SELECT queries, get the result set
   if (mysqli_stmt_field_count($stmt) > 0) {
-      $result = mysqli_stmt_get_result($stmt);
+    $result = mysqli_stmt_get_result($stmt);
   }
 
   // Close the statement
