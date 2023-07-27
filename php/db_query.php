3d41098bed8f5fbb09d7189434e9bc21f691c528
David L Bueno <davidlbueno@gmail.com>
2023-07-27 13:04:02 -0400

    handle param queries

--- php/db_query.php
+++ php/db_query.php
@@ -1,9 +1,32 @@
 <?php
-function query($conn, $query) {
-  $result = mysqli_query($conn, $query);
-  if (!$result) {
-    error_log("Query failed: " . mysqli_error($conn) . ". Query: " . $query);
-    die("An error occurred: " . mysqli_error($conn));
+function query($conn, $query, $params = []) {
+  // Prepare the SQL statement
+  $stmt = mysqli_prepare($conn, $query);
+  if (!$stmt) {
+      error_log("Statement preparation failed: " . mysqli_error($conn));
+      die("An error occurred: " . mysqli_error($conn));
   }
+
+  // Bind parameters to the SQL statement
+  if (!empty($params)) {
+      $types = str_repeat('s', count($params)); // assumes all parameters are strings
+      mysqli_stmt_bind_param($stmt, $types, ...$params);
+  }
+
+  // Execute the SQL statement
+  $result = mysqli_stmt_execute($stmt);
+  if (!$result) {
+      error_log("Query execution failed: " . mysqli_stmt_error($stmt));
+      die("An error occurred: " . mysqli_stmt_error($stmt));
+  }
+
+  // For SELECT queries, get the result set
+  if (mysqli_stmt_field_count($stmt) > 0) {
+      $result = mysqli_stmt_get_result($stmt);
+  }
+
+  // Close the statement
+  mysqli_stmt_close($stmt);
+
   return $result;
 }
