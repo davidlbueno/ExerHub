5f6b0f24b65be5bb76d4bb1425e030671ff1e7bf
David L Bueno <davidlbueno@gmail.com>
2023-08-04 23:35:47 -0400

    correct php db vars

--- php/db_connect.php
+++ php/db_connect.php
@@ -3,10 +3,10 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 //require_once 'config.php';
-$db_host = getenv('DB_HOST');
-$db_user = getenv('DB_USER');
-$db_password = getenv('DB_PASSWORD');
-$db_name = getenv('DB_NAME');
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$database = getenv('DB_NAME');
 
 // Create connection
 $conn = mysqli_connect($host, $user, $password, $database);
