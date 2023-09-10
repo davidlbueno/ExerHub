ef41f094806a35f6ec951f496d686c4d86fd8fa0
David L Bueno <davidlbueno@gmail.com>
2023-09-10 19:41:30 -0400

    check user creds for logs

--- edit_workout_log.php
+++ edit_workout_log.php
@@ -4,6 +4,21 @@ include 'php/session.php';
 require_once 'php/header.php';
 require_once 'php/db_connect.php';
 require_once 'php/db_query.php';
+
+$logId = $_GET['log_id'];
+$userId = $_SESSION['user_id'];
+$is_admin = $_SESSION['is_admin'];
+
+// Check if the user is authorized to view this log
+$authQuery = "SELECT user_id FROM workout_logs WHERE id = $logId";
+$authResult = query($conn, $authQuery);
+$authRow = mysqli_fetch_assoc($authResult);
+
+if ($authRow['user_id'] !== $userId && !$is_admin) {
+  echo "You can only view logs for your own workouts.";
+  exit;
+}
+
 ?>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
@@ -11,9 +26,6 @@ require_once 'php/db_query.php';
 <body class="dark">
   <main class="container">
     <?php
-    $is_admin = $_SESSION['is_admin'];
-    $logId = $_GET['log_id'];
-
     // Fetch existing log entries for this logId
     $logItemsQuery = "SELECT * FROM workout_log_items WHERE workout_log_id = $logId";
     $logItemsResult = query($conn, $logItemsQuery);
@@ -45,10 +57,11 @@ require_once 'php/db_query.php';
       }
 
       // Determine the background color based on exercise type
+      $bgColor = "";
       if ($exerciseType === "Rest") {
         $bgColor = "style='background-color: darkgreen;'";
-      } else {
-        $bgColor = "";
+      } elseif ($exerciseType === "Warmup") {
+        $bgColor = "style='background-color: darkblue;'";
       }
 
       echo "<tr $bgColor>";
