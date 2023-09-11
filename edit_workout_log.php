d125190c878650d1c6d3b4484b5f489dee1be03a
David L Bueno <davidlbueno@gmail.com>
2023-09-10 20:29:52 -0400

    WIP: Format edit workout log page

--- edit_workout_log.php
+++ edit_workout_log.php
@@ -10,7 +10,7 @@ $userId = $_SESSION['user_id'];
 $is_admin = $_SESSION['is_admin'];
 
 // Check if the user is authorized to view this log
-$authQuery = "SELECT user_id FROM workout_logs WHERE id = $logId";
+$authQuery = "SELECT user_id, start_time, end_time, workout_id FROM workout_logs WHERE id = $logId";
 $authResult = query($conn, $authQuery);
 $authRow = mysqli_fetch_assoc($authResult);
 
@@ -19,12 +19,31 @@ if ($authRow['user_id'] !== $userId && !$is_admin) {
   exit;
 }
 
+// Fetch workout name
+$workoutId = $authRow['workout_id'];
+$workoutQuery = "SELECT name FROM workouts WHERE id = $workoutId";
+$workoutResult = query($conn, $workoutQuery);
+$workoutRow = mysqli_fetch_assoc($workoutResult);
+$workoutName = $workoutRow['name'];
+
+// Fetch and format the start time and end time
+$startTime = $authRow['start_time'];
+$endTime = $authRow['end_time'];
+$duration = strtotime($endTime) - strtotime($startTime);
+$length = gmdate("H:i:s", $duration);
+
 ?>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
 <body class="dark">
   <main class="container">
+    <h4><?php echo $workoutName; ?></h4>
+    <div style="display: flex; justify-content: space-between;">
+      <p>Date: <?php echo date("Y-m-d", strtotime($startTime)); ?></p>
+      <p>Time: <?php echo date("H:i:s", strtotime($startTime)); ?></p>
+      <p>Duration: <?php echo $length; ?></p>
+    </div>
     <?php
     // Fetch existing log entries for this logId
     $logItemsQuery = "SELECT * FROM workout_log_items WHERE workout_log_id = $logId";
@@ -34,7 +53,7 @@ if ($authRow['user_id'] !== $userId && !$is_admin) {
     echo "<input type='hidden' name='log_id' value='$logId'>";
 
     echo "<table>";
-    echo "<tr><th>Exercise Type</th><th>Exercise Name</th><th>Time</th><th>Reps</th></tr>";
+    echo "<tr><th style='padding: 5px;'>Type</th><th style='padding: 5px;'>Exercise</th><th>Time</th><th style='padding: 5px;'>Reps</th></tr>";
 
     while ($logItemRow = mysqli_fetch_assoc($logItemsResult)) {
       $exerciseType = $logItemRow['exercise_type'];
@@ -65,15 +84,15 @@ if ($authRow['user_id'] !== $userId && !$is_admin) {
       }
 
       echo "<tr $bgColor>";
-      echo "<td><input type='text' name='exercise_type[]' value='$exerciseType'></td>";
-      echo "<td><input type='text' name='exercise_name[]' value='$exerciseName'></td>";  // Display the exercise name
-      echo "<td><input type='text' name='exercise_time[]' value='$exerciseTime'></td>";
-      echo "<td><input type='text' name='reps[]' value='$reps'></td>";
+      echo "<td style='padding: 0px;'><input type='text' name='exercise_type[]' value='$exerciseType'></td>";
+      echo "<td style='padding: 0px;'><input type='text' name='exercise_name[]' value='$exerciseName'></td>";
+      echo "<td style='padding: 0px;'><input type='text' name='exercise_time[]' value='$exerciseTime'></td>";
+      echo "<td style='padding: 0px;'><input type='text' name='reps[]' value='$reps'></td>";
       echo "</tr>";
     }
 
-    echo "</table>";
-    echo "<input type='submit' value='Update Log'>";
+    echo "</table><br>";   
+    echo "<input type='submit' value='Update Log' class='btn'>";
     echo "</form>";
     ?>
   </main>
